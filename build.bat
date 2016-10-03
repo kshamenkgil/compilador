dbf1b3ce8727e39d17dc813d45ea1dd3bb7ded86
Kshamenk Gil <Kshamenk Gil>
2016-10-03 18:24:14 -0300

    Fix cota constante

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
-c:\GnuWin32\bin\flex src\Lexico.l
-c:\GnuWin32\bin\bison -dyv src\Sintactico.y
-c:\MinGW\bin\gcc.exe src\lex.yy.c src\y.tab.c -o bin\TPEntregable.exe
+cd src
+flex Lexico.l
+bison -dyv Sintactico.y
+cd ..
+gcc.exe src/lex.yy.c src/y.tab.c -o bin/Primera.exe
 
 del src\lex.yy.c
 del src\y.tab.c
